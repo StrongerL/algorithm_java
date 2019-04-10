e03ff5b9839f78a949cac35b3be4b3aebc334b6e
StrongerL <871682558@qq.com>
2019-04-11 01:10:36 +0800

    链表两数相加

--- src/Tmp.java
+++ src/Tmp.java
@@ -1,5 +1,4 @@
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+
 
 public class Tmp {
 
@@ -7,35 +6,10 @@ public class Tmp {
 
     public static void main(String[] args) {
 
-
-        Map<String, String> map = new LinkedHashMap<>();
-        map.put("1", "a");
-        map.put("2", "b");
-        map.put("3", "c");
-        map.put("4", "d");
-
-        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-
-        System.out.println("以下是accessOrder=true的情况:");
-
-        map = new LinkedHashMap<String, String>(10, 0.75f, true);
-        map.put("1", "a");
-        map.put("2", "b");
-        map.put("3", "c");
-        map.put("4", "d");
-        map.get("1");//2移动到了内部的链表末尾
-        map.get("4");//4调整至末尾
-        map.put("3", "e");//3调整至末尾
-        map.put(null, null);//插入两个新的节点 null
-        map.put("5", null);//5
-        iterator = map.entrySet().iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-
+        int a = 9, b, c = 3;
+        System.out.println(a);
+//        System.out.println(b);
+        System.out.println(c);
 
 
     }
