c6a320ce50cbce1fe9b303a509d0a8fb691759bc
StrongerL <871682558@qq.com>
2021-08-23 10:48:52 +0800

    更新readme文件

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,80 @@
-import java.util.*;
+//给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。 
+//
+// 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: [3,2,1,5,6,4] 和 k = 2
+//输出: 5
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+//输出: 4 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= k <= nums.length <= 104 
+// -104 <= nums[i] <= 104 
+// 
+// Related Topics 数组 分治 快速选择 排序 堆（优先队列） 
+// 👍 1235 👎 0
 
-public class Solution {
 
+import java.util.Random;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
 
     public static void main(String[] args) {
-
-        System.out.println(null == null);
-
-
+        Solution s = new Solution();
+        System.out.println(s.findKthLargest(new int[]{3,2,3,1,2,4,5,5,6}, 4));
     }
+
+    public int findKthLargest(int[] nums, int k) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums[nums.length - k];
+    }
+
+    private void quickSort(int[] nums, int left, int right) {
+        if (left > right) {
+            return;
+        }
+        int pivot = partition(nums, left, right);
+        quickSort(nums, left, pivot - 1);
+        quickSort(nums, pivot + 1, right);
+    }
+
+    private int partition(int[] nums, int left, int right) {
+//        Random random = new Random();
+//        int idx = left + random.nextInt(right - left + 1);
+//        int tmp = nums[left];
+//        nums[left] = nums[idx];
+//        nums[idx] = tmp;
+
+        int pivot = nums[left];
+        while (left < right) {
+            while (left < right && nums[right] >= pivot) {
+                right--;
+            }
+            nums[left] = nums[right];
+            while (left < right && nums[left] < pivot) {
+                left++;
+            }
+            nums[right] = nums[left];
+        }
+        nums[left] = pivot;
+        return left;
+    }
+
+
 }
+//leetcode submit region end(Prohibit modification and deletion)
