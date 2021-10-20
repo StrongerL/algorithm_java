26dc8b1d374a8debd87896f7790add1e36d6768d
StrongerL <871682558@qq.com>
2021-10-20 17:10:26 +0800

    【11】盛最多水的容器

--- src/Tmp.java
+++ src/Tmp.java
@@ -6,6 +6,11 @@ public class Tmp {
 
     public static void main(String[] args) {
 
+        int[] a = new int[]{0, 1, 2};
+        int i = 1;
+//        System.out.println(i * a[i++]);
+        System.out.println(a[i++] * i);
+
 
 
 
