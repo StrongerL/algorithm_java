953f636dc19ce4ae6d7f2f60d92060bd62627b78
StrongerL <871682558@qq.com>
2021-10-22 17:34:49 +0800

    【10】正则表达式匹配

--- src/Tmp.java
+++ src/Tmp.java
@@ -6,12 +6,8 @@ public class Tmp {
 
     public static void main(String[] args) {
 
-        int[] a = new int[]{0, 1, 2};
-        int i = 1;
-//        System.out.println(i * a[i++]);
-        System.out.println(a[i++] * i);
-
-
+        boolean a = true;
+        boolean c = true;
 
 
     }
