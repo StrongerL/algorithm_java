55ffe7901d33df4b610506149c99c45701986e58
StrongerL <871682558@qq.com>
2021-11-03 13:15:01 +0800

    【31】下一个排列 【32】最长有效括号

--- src/Solution.java
+++ src/Solution.java
@@ -1,80 +1,28 @@
-//给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。 
-//
-// 异位词 指字母相同，但排列不同的字符串。 
-//
-// 
-//
-// 示例 1: 
-//
-// 
-//输入: s = "cbaebabacd", p = "abc"
-//输出: [0,6]
-//解释:
-//起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
-//起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
-// 
-//
-// 示例 2: 
-//
-// 
-//输入: s = "abab", p = "ab"
-//输出: [0,1,2]
-//解释:
-//起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
-//起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
-//起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
-// 
-//
-// 
-//
-// 提示: 
-//
-// 
-// 1 <= s.length, p.length <= 3 * 10⁴ 
-// s 和 p 仅包含小写字母 
-// 
-// Related Topics 哈希表 字符串 滑动窗口 👍 612 👎 0
+import java.util.Arrays;
 
-
-import java.util.*;
-import java.util.LinkedList;
-import java.util.List;
-
-//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 1;
+        while (i > 0 && nums[i] <= nums[i - 1]) i--;
+        if (i != 0) {
+            int j = n - 1;
+            while (j >= i && nums[j] <= nums[i - 1]) j--;
+            swap(nums, i - 1, j);
+        }
+        for (int k = i; k < (n + i)/2; k++) swap(nums, k, (n - 1) - (k - i));
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.longestPalindrome("babad"));
-    }
-
-    public String longestPalindrome(String s) {
-        String ans = "";
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j <= i + 1; j++) {
-                String current = palindrome(s, i, j);
-                if (current.length() > ans.length()) {
-                    ans = current;
-                }
-            }
-        }
-        return ans;
-    }
-
-    private String palindrome(String s, int i, int j) {
-        while (i >= 0 && j < s.length()) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i--; j++;
-            } else {
-                break;
-            }
-        }
-        i++; j--;
-        if (i >= 0 && j < s.length() && i <= j) {
-            return s.substring(i, j + 1);
-        } else {
-            return "";
-        }
+        int[] nums = new int[]{5, 4, 7, 5, 3, 2};
+        s.nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
